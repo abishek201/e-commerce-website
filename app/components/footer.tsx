77b00a1ca2731d9ba1613660e61357e364223390
abishek201 <abhishek.lokesh305@gmail.com>
2026-08-20 19:28:31 +0530

    myy website is finshed

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,39 +1,27 @@
+import "../css-components/footer.css";
+
 export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="contact">
+          <h3>STAY CONNECTED</h3>
+          <h4>abhi@gmail.com</h4>
+          <h4>PH: 9353802818</h4>
+        </div>
 
-    return (
-        <>
-        <div className="footer">
-            <div className="footer-container">
-                <div className="contact">
-                    <h3>STAYY CONNECTED</h3>
-                    <h4>abhi@gmail.com</h4>
-                    <h4>PH:9353802818</h4>
-                </div>
-                <div className="logo-contact">
-                   <ul>
-                    <li>insta</li>
-                    <li>youtube</li>
-                    <li>mail</li>
-                   </ul>
-                </div>
-               
+        <div className="logo-contact">
+          <ul>
+            <li>Instagram</li>
+            <li>YouTube</li>
+            <li>Mail</li>
+          </ul>
+        </div>
+      </div>
 
-            </div>
-             <div className="brand-name">
-                <p>BRAND NAME</p>
-
-            </div>
-            </div>
-            
-
-
-        </>
-
-
-
-
-    );
-
-
-
+      <div className="brand-name">
+        <p>BRAND NAME</p>
+      </div>
+    </footer>
+  );
 }
